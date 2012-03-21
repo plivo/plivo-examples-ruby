34fbe6d150fd960c61b88b120331ca73edb1b930
tsudot <kunalkerkar@gmail.com>
2012-03-21 16:12:36 +0530

    Syntax fix

--- plivo_number.rb
+++ plivo_number.rb
@@ -11,42 +11,42 @@ response = p.get_numbers()
 
 # Get one rented number
 params = {
-        'number': '1212121212',
+        'number'=> '1212121212',
 }
 response = p.get_number(params)
 
 # Search number
 params = {
-    'contains': '43',
+    'contains'=> '43',
 }
 response = p.search_numbers(params)
 
 
 # Rent number
 params = {
-        'number': '1212121212',
+        'number'=> '1212121212',
 }
 response = p.rent_number(params)
 
 
 # Link application
 params = {
-        'number': '1212121212',
-        'app_id': 'SACSSSASDDDDDD',
+        'number'=> '1212121212',
+        'app_id'=> 'SACSSSASDDDDDD',
 }
 response = p.link_application_number(params)
 
 
 # Unlink application
 params = {
-        'number': '1212121212',
+        'number'=> '1212121212',
 }
 response = p.unlink_application_number(params)
 
 
 # Get subaccount numbers
 params = {
-        'subauth_id': 'XXXXXXXXXXXXXXXXXXX',
+        'subauth_id'=> 'XXXXXXXXXXXXXXXXXXX',
 }
 response = p.get_subaccount_numbers(params)
 
