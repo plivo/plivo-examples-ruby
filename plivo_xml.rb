3dbe86f584b814b6653249b559d082d22f99e256
tsudot <kunalkerkar@gmail.com>
2012-04-04 10:42:38 +0530

    XML generation example

---
+++ plivo_xml.rb
@@ -0,0 +1,46 @@
+require 'plivo'
+
+# Dial XML
+dial_callerId = '1XXXXXXXX'
+dial_number = '120XXXXX'
+r = Response.new()
+d = r.addDial({'callerId' => dial_callerId})
+d.addNumber(dial_number)
+r.to_xml()
+
+#Output:
+#<Response>
+#<Dial callerId='1XXXXXXXX'>
+#<Number>120XXXXX</Number>
+#</Dial>
+#</Response>
+
+
+# Speak XML
+
+r = Response.new()
+r.addWait({'length' => 2 })
+r.addSpeak('Hi')
+r.to_xml()
+
+#Output:
+#<Response>
+#<Wait length='2'/>
+#<Speak>Hi</Speak>
+#</Response>
+
+
+# Play XML
+play_url = 'http://example.com/bonjo.mp3'
+play_loop = '2'
+
+r = Response.new()
+r.addPlay(play_url, {'loop' => play_loop})
+r.to_xml()
+
+#Output:
+#<Response>
+#<Play loop='2'>http://example.com/bonjo.mp3</Play>
+#</Response>
+
+
