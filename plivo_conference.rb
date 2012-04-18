25318a6400041dc089350e253617f56f322db6ef
Michael Ricordeau <mike@plivo.com>
2012-04-18 05:48:41 +0200

    replace conference_id by conference_name

--- plivo_conference.rb
+++ plivo_conference.rb
@@ -11,32 +11,32 @@ response = p.get_live_conference()
 
 
 # Get live conference
-params = {'conference_id' => 'XXXXXXXXXXXXXXXXX', 
+params = {'conference_name' => 'XXXXXXXXXXXXXXXXX', 
         }   
 response = p.get_live_conference(params)
 
 
 # Hangup conference
-params = {'conference_id' => 'XXXXXXXXXXXXXXXXX', 
+params = {'conference_name' => 'XXXXXXXXXXXXXXXXX', 
         }   
 response = p.hangup_conference(params)
 
 
 # Hangup member
-params = {'conference_id' => 'XXXXXXXXXXXXXXXXX', 
+params = {'conference_name' => 'XXXXXXXXXXXXXXXXX', 
           'member_id' => 'MMMMMMMMMMMMMMM',
         }   
 response = p.hangup_member(params)
 
 
 # Record conference
-params = {'conference_id' => 'XXXXXXXXXXXXXXXXX', 
+params = {'conference_name' => 'XXXXXXXXXXXXXXXXX', 
         }   
 response = p.record_conference(params)
 
 
 # Stop recording conference
-params = {'conference_id' => 'XXXXXXXXXXXXXXXXX', 
+params = {'conference_name' => 'XXXXXXXXXXXXXXXXX', 
         }   
 response = p.stop_record_conference(params)
 
