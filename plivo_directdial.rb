71c92eeaa6453357c8594895b414143ab04d1fe4
Rakesh Kadarla <rakesh@plivo.com>
2012-09-04 05:12:29 -0700

    example for direct dial app answer url

---
+++ plivo_directdial.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'plivo'
+require 'sinatra'
+
+include Plivo
+
+get '/dial' do
+  to_number = params[:To]
+  from_number = params[:CLID]
+  if not from_number
+    from_number = params[:From] ? params[:From] : ''
+  end
+  caller_name = params[:CallerName] ? params[:CallerName] : ''
+
+  resp = Response.new()
+  if not to_number
+    resp.addHangup()
+  else
+    if to_number[0, 4] == "sip:"
+      d = resp.addDial({'callerName' => caller_name})
+      d.addUser(to_number)
+    else
+      d = resp.addDial({'callerId' => from_number})
+      d.addNumber(to_number)
+    end
+  end
+  content_type :xml 
+  "text/xml"
+  resp.to_xml()
+end
