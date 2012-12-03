c3f672ae27e2157f5b733f68a4746b025a09e292
kaydarla <rakesh@plivo.com>
2012-12-03 10:49:12 -0600

    added an example for GetDigits

--- plivo_xml.rb
+++ plivo_xml.rb
@@ -10,12 +10,13 @@ d = r.addDial({'callerId' => dial_callerId})
 d.addNumber(dial_number)
 puts r.to_xml()
 
-#Output:
-#<Response>
-#<Dial callerId='1XXXXXXXX'>
-#<Number>120XXXXX</Number>
-#</Dial>
-#</Response>
+# Output:
+# <?xml version="1.0" encoding="utf-8" ?>
+# <Response>
+#   <Dial callerId='1XXXXXXXX'>
+#     <Number>120XXXXX</Number>
+#   </Dial>
+# </Response>
 
 
 # Speak XML
@@ -25,11 +26,12 @@ r.addWait({'length' => 2 })
 r.addSpeak('Hi')
 puts r.to_xml()
 
-#Output:
-#<Response>
-#<Wait length='2'/>
-#<Speak>Hi</Speak>
-#</Response>
+# Output:
+# <?xml version="1.0" encoding="utf-8" ?>
+# <Response>
+#   <Wait length='2'/>
+#   <Speak>Hi</Speak>
+# </Response>
 
 
 # Play XML
@@ -40,9 +42,27 @@ r = Response.new()
 r.addPlay(play_url, {'loop' => play_loop})
 puts r.to_xml()
 
-#Output:
-#<Response>
-#<Play loop='2'>http://example.com/bonjo.mp3</Play>
-#</Response>
+# Output:
+# <?xml version="1.0" encoding="utf-8" ?>
+# <Response>
+#   <Play loop='2'>http://example.com/bonjo.mp3</Play>
+# </Response>
 
 
+# GetDigits XML
+receive_url = 'http://example.com/receivedigits'
+
+r = Response.new()
+d = r.addGetDigits({'action' => receive_url, 'method' => 'GET'})
+d.addSpeak('Press one for English, press two for Spanish')
+r.addSpeak('No input received. Good bye')
+puts r.to_xml()
+
+# Output:
+# <?xml version="1.0" encoding="utf-8" ?>
+# <Response>
+#   <GetDigits action='http://example.com/receivedigits' method='GET'>
+#     <Speak>Press one for English, press two for Spanish</Speak>
+#   </GetDigits>
+#   <Speak>No input received. Good bye</Speak>
+# </Response>
