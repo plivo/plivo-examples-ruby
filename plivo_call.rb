30969f86eac3301b91e60ef9c3475b31eb0fc291
tsudot <kunalkerkar@gmail.com>
2013-03-20 02:27:25 +0530

    Removing uncessary functions

--- plivo_call.rb
+++ plivo_call.rb
@@ -17,24 +17,6 @@ params = {'to' => '121212121212',
            'hangup_url' => 'http://example.com/HangupUrl',
         }
 response = p.make_call(params)
-
-# Get live calls
-response = p.get_live_calls()
-
-
-# Get details of a live call
-params = {'call_uuid' => 'XXXXXXXXXXXXXXXXXXXXXXX'}
-response = p.get_live_call(params)
-
-
-# Transfer calls
-params = {'transfer_url' => 'http://example.com/TransferUrl'}
-response = p.transfer_call(params)
-
-
-# Hangup calls
-params = {'call_uuid' => 'XXXXXXXXXXXXXXXXXXXXXXX'}
-response = p.hangup_call(params)
-
+print response
 
 
