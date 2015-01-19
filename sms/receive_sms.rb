559d964bd1505a9c534f620af48e057d5edc1252
Ramya <ramya@Ramya.local>
2015-01-19 15:20:47 +0530

    Receive an sms

---
+++ sms/receive_sms.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'plivo'
+include Plivo
+
+post '/message/' do
+
+	# The phone number of the person who sent the SMS
+	from_number = params[:To]
+
+	# Your Plivo number that will receive the SMS
+	to_number = params[:From]
+
+	# The text which was received on your Plivo number
+	text = params[:Text]
+
+	# Output the text which was received, you could
+	# also store the text in a database.
+	puts "Message received from #{from_number} : #{ text }"
+
+	"Message received"
+end
