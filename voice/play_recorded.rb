5421431d0cca3bdeee05111a3f28c4a98f06cc92
Ramya <ramya@plivo.com>
2015-02-09 17:19:53 +0530

    Play recorded tune

---
+++ voice/play_recorded.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'plivo'
+include Plivo
+
+get '/play' do
+    r = Response.new()
+    r.addPlay("https://s3.amazonaws.com/plivocloud/Trumpet.mp3")
+
+    puts r.to_xml()
+    content_type 'text/xml'
+    return r.to_s()
+end
+
+=begin
+Sample Play XML
+<Response>
+   <Play>https://s3.amazonaws.com/plivocloud/Trumpet.mp3</Play>
+</Response>
+=end
